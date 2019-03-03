453d3ced8fcc8d7944980f5439adeee0c1ee379c
atrum <Hekchanel@mail.ru>
2019-03-04 00:46:56 +0300

    ifdef style fixes

--- include/substring.hpp
+++ include/substring.hpp
@@ -1,7 +1,7 @@
 // Copyright 2018 Your Name <your_email>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#ifndef INCLUDE_SUBSTRING_HPP_
+#define INCLUDE_SUBSTRING_HPP_
 
 #include <string>
 #include <functional>
@@ -16,4 +16,4 @@ size_t rk_find(const std::string &str, const std::string &substr);
 /// Поиск подстроки в строке алгоритмом Кнута—Морриса—Пратта
 size_t kmp_find(const std::string &str, const std::string &substr);
 
-#endif // INCLUDE_HEADER_HPP_
+#endif // INCLUDE_SUBSTRING_HPP_
